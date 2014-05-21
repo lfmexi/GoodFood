ff07a3ce121030e7f937c1190ff04f9f52339e65
luisMoralesMejicanos <lfmexi@gmail.com>
2014-05-21 17:34:30 -0600

    Agregado el mockup de comentario de receta

--- src/com/example/goodfood/BuscaReceta.java
+++ src/com/example/goodfood/BuscaReceta.java
@@ -2,7 +2,15 @@ package com.example.goodfood;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class BuscaReceta extends Activity {
 
@@ -10,13 +18,42 @@ public class BuscaReceta extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_busca_receta);
+		Button buscar=(Button)this.findViewById(R.id.botonBuscarReceta);
+		lista=(ListView)this.findViewById(R.id.listaResultados);
+		
+		buscar.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				buscarOnClick();
+			}
+		});
+		
+		lista.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				listaOnClick(arg1);
+			}
+		});
 	}
 
+	private void buscarOnClick(){
+		String listado[]=new String[]{
+				"Ingresar nueva receta","Buscar recetas","Mis recetas","Ejercicios recomendados","Perfil"
+		};
+		lista.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,listado));
+	}
+	
+	private void listaOnClick(View v){
+		this.startActivity(new Intent(this,VisualizaReceta.class));
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.busca_receta, menu);
 		return true;
 	}
-
+	private ListView lista;
 }
--- src/com/example/goodfood/VisualizaReceta.java
+++ src/com/example/goodfood/VisualizaReceta.java
@@ -2,7 +2,14 @@ package com.example.goodfood;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.TextView;
 
 public class VisualizaReceta extends Activity {
 
@@ -10,8 +17,35 @@ public class VisualizaReceta extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_visualiza_receta);
+		nombreReceta=(TextView)this.findViewById(R.id.labelNombreReceta);
+		barra=(RatingBar)this.findViewById(R.id.ratingBar1);
+		botonComenta=(Button)this.findViewById(R.id.botonComentar);
+		
+		barra.setOnRatingBarChangeListener(new OnRatingBarChangeListener(){
+			@Override
+			public void onRatingChanged(RatingBar arg0, float arg1, boolean arg2) {
+				// TODO Auto-generated method stub
+				barraOnRatingBarChange(arg1);
+			}
+		});
+		
+		botonComenta.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				botonComentaOnClick();
+			}
+		});
+		
+	}
+	
+	private void botonComentaOnClick(){
+		this.startActivity(new Intent(this,ComentarioReceta.class));
 	}
 
+	private void barraOnRatingBarChange(float valorNuevo){
+		
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -19,4 +53,7 @@ public class VisualizaReceta extends Activity {
 		return true;
 	}
 
+	private TextView nombreReceta;
+	private RatingBar barra;
+	private Button botonComenta;
 }
